panglesd/dune

commit 1377833b1167d420dff78e343cb57e6532ae1724

Clarify that a string represents the name of an mld
Signed-off-by: Paul-Elliot <peada@free.fr>

src/dune_rules/odoc.ml
@@ -203,20 +203,22 @@ let odoc_ext = ".odoc"
 module Mld : sig
   type t
 
-  val create : Path.Build.t * string -> t
+  val create : path:Path.Build.t -> name:string -> t
   val odoc_file : doc_dir:Path.Build.t -> t -> Path.Build.t
   val odoc_input : t -> Path.Build.t
 end = struct
-  type t = Path.Build.t * string
+  type t =
+    { path : Path.Build.t
+    ; name : string
+    }
 
-  let create t = t
+  let create ~path ~name = { path; name }
 
-  let odoc_file ~doc_dir (_path, name) =
-    let name = Filename.remove_extension name in
+  let odoc_file ~doc_dir { name; _ } =
     Path.Build.relative doc_dir (sprintf "page-%s%s" name odoc_ext)
   ;;
 
-  let odoc_input (p, _) = p
+  let odoc_input { path; _ } = path
 end
 
 module Flags = struct
@@ -671,7 +673,8 @@ let mlds sctx pkg =
   Packages.mlds sctx pkg
   >>| List.filter_map ~f:(fun mld ->
     match Path.Local.explode mld.Doc_sources.in_doc with
-    | [ name ] when Filename.extension name = ".mld" -> Some (mld.path, name)
+    | [ name ] when Filename.extension name = ".mld" ->
+      Some (mld.path, Filename.remove_extension name)
     | _ ->
       User_warning.emit
         [ Pp.textf
@@ -691,11 +694,11 @@ let odoc_artefacts sctx target =
       let+ mlds = mlds sctx pkg in
       let mlds = check_mlds_no_dupes ~pkg ~mlds in
       Filename.Map.update mlds "index.mld" ~f:(function
-        | None -> Some (Paths.gen_mld_dir ctx pkg ++ "index.mld", "index.mld")
+        | None -> Some (Paths.gen_mld_dir ctx pkg ++ "index.mld", "index")
         | Some _ as s -> s)
     in
-    Filename.Map.to_list_map mlds ~f:(fun _ mld ->
-      Mld.create mld |> Mld.odoc_file ~doc_dir:dir |> create_odoc ctx ~target)
+    Filename.Map.to_list_map mlds ~f:(fun _ (path, name) ->
+      Mld.create ~path ~name |> Mld.odoc_file ~doc_dir:dir |> create_odoc ctx ~target)
   | Lib lib ->
     let info = Lib.Local.info lib in
     let obj_dir = Lib_info.obj_dir info in
@@ -961,10 +964,10 @@ let setup_package_odoc_rules sctx ~pkg =
      back to a package name here. Need to try and change that one day. *)
   let* odocs =
     Filename.Map.values mlds
-    |> Memo.parallel_map ~f:(fun mld ->
+    |> Memo.parallel_map ~f:(fun (path, name) ->
       compile_mld
         sctx
-        (Mld.create mld)
+        (Mld.create ~path ~name)
         ~pkg
         ~doc_dir:(Paths.odocs ctx (Pkg pkg))
         ~includes:(Action_builder.return []))

src/dune_rules/odoc_new.ml
@@ -680,7 +680,7 @@ module Artifact : sig
   val module_name : t -> Module_name.t option
   val name : t -> string
   val make_module : Context.t -> all:bool -> Index.t -> Path.t -> visible:bool -> t
-  val external_mld : Context.t -> Index.t -> Path.t * string -> t
+  val external_mld : Context.t -> Index.t -> Path.t -> name:string -> t
   val index : Context.t -> all:bool -> Index.t -> t
 end = struct
   type artifact_ty =
@@ -755,30 +755,28 @@ end = struct
     v ~source ~odoc ~html_dir ~html_file:html ~ty:(Module visible)
   ;;
 
-  let int_make_mld ctx ~all index (source, name) ~is_index =
-    let basename = name |> Filename.remove_extension in
+  let int_make_mld ctx ~all index source ~name ~is_index =
     let odoc =
       (if is_index then Index.obj_dir ctx ~all index else Index.odoc_dir ctx ~all index)
-      ++ ("page-" ^ basename ^ ".odoc")
+      ++ ("page-" ^ name ^ ".odoc")
     in
     let html_dir = Index.html_dir ctx ~all index in
-    let html =
-      html_dir ++ if is_index then "index.html" else sprintf "%s.html" basename
-    in
+    let html = html_dir ++ if is_index then "index.html" else sprintf "%s.html" name in
     v ~source ~odoc ~html_dir ~html_file:html ~ty:Mld
   ;;
 
-  let external_mld ctx index (source, name) =
-    int_make_mld ctx ~all:true index (source, name) ~is_index:false
+  let external_mld ctx index source ~name =
+    int_make_mld ctx ~all:true index source ~name ~is_index:false
   ;;
 
   let index ctx ~all index =
+    let filename = Index.mld_filename index in
     let source =
-      let filename = Index.mld_filename index in
       let dir = Index.obj_dir ctx ~all index in
-      Path.build (dir ++ filename), filename
+      Path.build (dir ++ filename)
     in
-    int_make_mld ctx ~all index source ~is_index:true
+    let name = Filename.remove_extension filename in
+    int_make_mld ctx ~all index source ~name ~is_index:true
   ;;
 end
 
@@ -1259,7 +1257,8 @@ let pkg_mlds sctx pkg =
     Packages.mlds sctx pkg
     >>| List.filter_map ~f:(fun mld ->
       match Path.Local.explode mld.Doc_sources.in_doc with
-      | [ name ] when Filename.extension name = ".mld" -> Some (Path.build mld.path, name)
+      | [ name ] when Filename.extension name = ".mld" ->
+        Some (Path.build mld.path, Filename.remove_extension name)
       | _ ->
         User_warning.emit
           [ Pp.textf
@@ -1298,7 +1297,7 @@ let pkg_artifacts sctx index pkg =
   let artifacts =
     let mlds_noindex = String.Map.filteri ~f:(fun i _ -> i <> "index.mld") mlds_map in
     String.Map.values mlds_noindex
-    |> List.map ~f:(fun mld -> Artifact.external_mld ctx index mld)
+    |> List.map ~f:(fun (path, name) -> Artifact.external_mld ctx index path ~name)
   in
   let index_file = String.Map.find mlds_map "index.mld" in
   Option.map ~f:fst index_file, artifacts

commit 8712ee16dd5d4727ddc59cd3b6d083dfed47fde5

Fix bug introduced by 1377833b1
Signed-off-by: Paul-Elliot <peada@free.fr>

src/dune_rules/odoc.ml
@@ -693,7 +693,7 @@ let odoc_artefacts sctx target =
     let+ mlds =
       let+ mlds = mlds sctx pkg in
       let mlds = check_mlds_no_dupes ~pkg ~mlds in
-      Filename.Map.update mlds "index.mld" ~f:(function
+      Filename.Map.update mlds "index" ~f:(function
         | None -> Some (Paths.gen_mld_dir ctx pkg ++ "index.mld", "index")
         | Some _ as s -> s)
     in
@@ -942,7 +942,7 @@ let package_mlds =
            let* mlds = mlds sctx pkg in
            let mlds = check_mlds_no_dupes ~pkg ~mlds in
            let ctx = Super_context.context sctx in
-           if Filename.Map.mem mlds "index.mld"
+           if Filename.Map.mem mlds "index"
            then Memo.return mlds
            else (
              let gen_mld = Paths.gen_mld_dir ctx pkg ++ "index.mld" in
@@ -952,7 +952,7 @@ let package_mlds =
                  sctx
                  (Action_builder.write_file gen_mld (default_index ~pkg entry_modules))
              in
-             Filename.Map.set mlds "index" (gen_mld, "index.mld"))))
+             Filename.Map.set mlds "index" (gen_mld, "index"))))
   in
   fun sctx ~pkg -> Memo.exec memo (sctx, pkg)
 ;;

src/dune_rules/odoc_new.ml
@@ -1255,8 +1255,8 @@ let pkg_mlds sctx pkg =
   if Package.Name.Map.mem pkgs pkg
   then
     Packages.mlds sctx pkg
-    >>| List.filter_map ~f:(fun (mld : Doc_sources.mld) ->
-      match Path.Local.explode mld.in_doc with
+    >>| List.filter_map ~f:(fun mld ->
+      match Path.Local.explode mld.Doc_sources.in_doc with
       | [ name ] when Filename.extension name = ".mld" ->
         Some (Path.build mld.path, Filename.remove_extension name)
       | _ ->
@@ -1295,11 +1295,11 @@ let pkg_artifacts sctx index pkg =
     check_mlds_no_dupes ~pkg ~mlds
   in
   let artifacts =
-    let mlds_noindex = String.Map.filteri ~f:(fun i _ -> i <> "index.mld") mlds_map in
+    let mlds_noindex = String.Map.filteri ~f:(fun i _ -> i <> "index") mlds_map in
     String.Map.values mlds_noindex
     |> List.map ~f:(fun (path, name) -> Artifact.external_mld ctx index path ~name)
   in
-  let index_file = String.Map.find mlds_map "index.mld" in
+  let index_file = String.Map.find mlds_map "index" in
   Option.map ~f:fst index_file, artifacts
 ;;
 