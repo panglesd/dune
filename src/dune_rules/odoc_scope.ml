f0dc3ad95970475f7af56a1ee322274791826a07
Paul-Elliot <peada@free.fr>
2026-04-13 10:01:43 +0200

    Redo 2cc67601ecbcc091062d05b28388616d75d99984 after rebase

--- src/dune_rules/odoc_scope.ml
+++ src/dune_rules/odoc_scope.ml
@@ -6,7 +6,10 @@ open Memo.O
 let file_key project =
   let name = Dune_project.name project in
   let root = Dune_project.root project in
-  let digest = Digest.generic (name, root) |> Digest.to_string in
+  let digest =
+    Digest.repr Repr.(pair Dune_project_name.repr Path.Source.repr) (name, root)
+    |> Digest.to_string
+  in
   String.take digest 12
 ;;
 
