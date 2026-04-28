648ad32f1ce56dd79b8fb738d079efc189b5ba9c
Arthur Wendling <arthur@tarides.com>
2026-04-28 10:57:21 +0200

    refactor(odoc): expand package_discovery with full Findlib walk and accessors

--- src/dune_rules/odoc/package_discovery.ml
+++ src/dune_rules/odoc/package_discovery.ml
@@ -7,7 +7,10 @@ type t =
   ; loc_of_pkg : Dune_package.External_location.t Package.Name.Map.t
   ; loc_of_lib : Dune_package.External_location.t Lib_name.Map.t
   ; libs_of_loc : (Dune_package.Lib.t * Lib.t) Lib_name.Map.t Ext_loc_map.t
+  ; pkg_of_lib : Package.Name.t Lib_name.Map.t
+  ; libs_of_pkg : Lib.t list Package.Name.Map.t
   ; mlds_of_pkg : (Path.t * string) list Package.Name.Map.t
+  ; module_files_of_lib : Path.t Module_name.Map.t Lib_name.Map.t
   }
 
 let empty =
@@ -15,7 +18,10 @@ let empty =
   ; loc_of_pkg = Package.Name.Map.empty
   ; loc_of_lib = Lib_name.Map.empty
   ; libs_of_loc = Ext_loc_map.empty
+  ; pkg_of_lib = Lib_name.Map.empty
+  ; libs_of_pkg = Package.Name.Map.empty
   ; mlds_of_pkg = Package.Name.Map.empty
+  ; module_files_of_lib = Lib_name.Map.empty
   }
 ;;
 
@@ -35,6 +41,41 @@ let mlds_of_dune_package (dpkg : Dune_package.t) =
             else None
           | Directory -> None)
       | _ -> [])
+;;
+
+(* Scan an install directory and return a [module_name -> file] map for
+   every [.cmti] (preferred) or [.cmt] found.  Mirrors
+   [odoc_new.modules_of_dir]. *)
+let module_files_in_dir dir =
+  Fs_memo.dir_contents (Path.as_outside_build_dir_exn dir)
+  >>| function
+  | Error _ -> Module_name.Map.empty
+  | Ok dc ->
+    let entries = Fs_memo.Dir_contents.to_list dc in
+    let extension_priority ext =
+      match Filename.Extension.Or_empty.to_string ext with
+      | ".cmti" -> Some 0
+      | ".cmt" -> Some 1
+      | _ -> None
+    in
+    List.fold_left entries ~init:Module_name.Map.empty ~f:(fun acc (name, kind) ->
+      match kind with
+      | Unix.S_REG ->
+        let ext = Filename.extension name in
+        (match extension_priority ext with
+         | None -> acc
+         | Some prio ->
+           let base = Filename.remove_extension name in
+           (match Module_name.of_string_user_error (Loc.none, base) with
+            | Error _ -> acc
+            | Ok mod_name ->
+              let path = Path.relative dir name in
+              Module_name.Map.update acc mod_name ~f:(function
+                | None -> Some (prio, path)
+                | Some (existing_prio, _) when prio < existing_prio -> Some (prio, path)
+                | Some _ as keep -> keep)))
+      | _ -> acc)
+    |> Module_name.Map.map ~f:snd
 ;;
 
 let build (ctx : Context.t) =
@@ -82,13 +123,25 @@ let build (ctx : Context.t) =
                   | Ok m -> m
                   | Error _ -> maps.loc_of_lib)
              in
-             let loc_of_pkg =
-               match pkg, location_opt with
-               | Some pkg_name, Some location ->
-                 (match Package.Name.Map.add maps.loc_of_pkg pkg_name location with
-                  | Ok m -> m
-                  | Error _ -> maps.loc_of_pkg)
-               | _ -> maps.loc_of_pkg
+             let loc_of_pkg, pkg_of_lib, libs_of_pkg =
+               match pkg with
+               | None -> maps.loc_of_pkg, maps.pkg_of_lib, maps.libs_of_pkg
+               | Some pkg_name ->
+                 let loc_of_pkg =
+                   match location_opt with
+                   | None -> maps.loc_of_pkg
+                   | Some location ->
+                     (match Package.Name.Map.add maps.loc_of_pkg pkg_name location with
+                      | Ok m -> m
+                      | Error _ -> maps.loc_of_pkg)
+                 in
+                 let pkg_of_lib = Lib_name.Map.set maps.pkg_of_lib name pkg_name in
+                 let libs_of_pkg =
+                   Package.Name.Map.update maps.libs_of_pkg pkg_name ~f:(function
+                     | None -> Some [ lib ]
+                     | Some libs -> Some (lib :: libs))
+                 in
+                 loc_of_pkg, pkg_of_lib, libs_of_pkg
              in
              let libs_of_loc =
                match location_opt with
@@ -101,13 +154,13 @@ let build (ctx : Context.t) =
                       | Ok libs -> Some libs
                       | Error _ -> Some libs))
              in
-             { maps with loc_of_lib; loc_of_pkg; libs_of_loc }))
+             { maps with loc_of_lib; loc_of_pkg; libs_of_loc; pkg_of_lib; libs_of_pkg }))
   in
-  let+ mlds_of_pkg =
+  let* mlds_of_pkg =
     Package.Name.Map.foldi
-      maps.loc_of_pkg
+      maps.libs_of_pkg
       ~init:(Memo.return Package.Name.Map.empty)
-      ~f:(fun pkg _loc acc ->
+      ~f:(fun pkg _libs acc ->
         let* acc = acc in
         Findlib.find_root_package findlib pkg
         >>| function
@@ -116,7 +169,24 @@ let build (ctx : Context.t) =
           let mlds = mlds_of_dune_package dpkg in
           if List.is_empty mlds then acc else Package.Name.Map.set acc pkg mlds)
   in
-  { maps with mlds_of_pkg }
+  (* Build module_files_of_lib: scan each installed lib's src_dir. *)
+  let* module_files_of_lib =
+    Lib_name.Map.foldi
+      maps.pkg_of_lib
+      ~init:(Memo.return Lib_name.Map.empty)
+      ~f:(fun lib_name _pkg acc ->
+        let* acc = acc in
+        Lib.DB.find installed_libs lib_name
+        >>= function
+        | None -> Memo.return acc
+        | Some lib ->
+          let src_dir = Lib_info.src_dir (Lib.info lib) in
+          let+ files = module_files_in_dir src_dir in
+          if Module_name.Map.is_empty files
+          then acc
+          else Lib_name.Map.set acc lib_name files)
+  in
+  Memo.return { maps with mlds_of_pkg; module_files_of_lib }
 ;;
 
 let create =
@@ -124,8 +194,35 @@ let create =
   fun ~context -> Memo.exec memo context
 ;;
 
+let package_of_library t lib =
+  (match Lib.Local.of_lib lib with
+   | Some _ ->
+     Code_error.raise
+       "Package_discovery.package_of_library called on local library"
+       [ "lib", Lib_name.to_dyn (Lib.name lib) ]
+   | None -> ());
+  Lib_name.Map.find t.pkg_of_lib (Lib.name lib)
+;;
+
+let libraries_of_package t pkg =
+  Package.Name.Map.find t.libs_of_pkg pkg |> Option.value ~default:[]
+;;
+
 let mlds_of_package t pkg =
   Package.Name.Map.find t.mlds_of_pkg pkg |> Option.value ~default:[]
+;;
+
+let module_source_file t ~lib ~module_name =
+  let lib_name = Lib.name lib in
+  let mod_name =
+    match Module_name.of_string_user_error (Loc.none, module_name) with
+    | Ok m -> Some m
+    | Error _ -> None
+  in
+  let open Option.O in
+  let* mod_name = mod_name in
+  let* lib_modules = Lib_name.Map.find t.module_files_of_lib lib_name in
+  Module_name.Map.find lib_modules mod_name
 ;;
 
 let findlib_paths t = t.findlib_paths
--- src/dune_rules/odoc/package_discovery.mli
+++ src/dune_rules/odoc/package_discovery.mli
@@ -4,7 +4,10 @@ module Ext_loc_map : Map.S with type key = Dune_package.External_location.t
 type t
 
 val create : context:Context.t -> t Memo.t
+val package_of_library : t -> Lib.t -> Package.Name.t option
+val libraries_of_package : t -> Package.Name.t -> Lib.t list
 val mlds_of_package : t -> Package.Name.t -> (Path.t * string) list
+val module_source_file : t -> lib:Lib.t -> module_name:string -> Path.t option
 val findlib_paths : t -> int Path.Map.t
 val location_of_package : t -> Package.Name.t -> Dune_package.External_location.t option
 val location_of_library : t -> Lib_name.t -> Dune_package.External_location.t option
