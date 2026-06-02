cd11fd8fdc77f8834a0ba702d9efd32debd000b8
Paul-Elliot <peada@free.fr>
2026-06-02 15:47:29 +0200

    refactor(odoc): add Odoc_target module (documentation targets)

---
+++ src/dune_rules/odoc/odoc_target.mli
@@ -0,0 +1,43 @@
+open Import
+
+module Doc_mode : sig
+  type t =
+    | Local_only
+    | Full
+
+  val subdir : t -> string
+  val all : t list
+end
+
+type page =
+  { name : string
+  ; pkg_libs : Lib.t list
+  }
+
+type asset =
+  { asset_name : string
+  ; asset_rel_path : string
+  }
+
+type mod_ =
+  { visible : bool
+  ; module_name : Module_name.t
+  }
+
+type impl =
+  { src_id : string
+  ; src_path : Path.t
+  ; module_name : Module_name.t
+  }
+
+type _ t =
+  | Lib : Package.Name.t * Lib.t -> mod_ t
+  | Private_lib : string * Lib.t -> mod_ t
+  | Pkg : Package.Name.t -> page t
+  | Toplevel : Doc_mode.t -> page t
+
+(** Existential wrapper for targets of any kind. *)
+type any = Any : 'a t -> any
+
+(** Compare two targets for sorting. Orders by target type then by name. *)
+val compare_any : any -> any -> Ordering.t
