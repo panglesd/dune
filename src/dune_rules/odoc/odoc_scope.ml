7b5261e3aca261b551b09e0d9c8bf7da860e69e0
Paul-Elliot <peada@free.fr>
2026-06-02 18:24:11 +0200

    feat(odoc): extend Odoc_scope with Scope_id and v3 lib_unique_name

--- src/dune_rules/odoc/odoc_scope.ml
+++ src/dune_rules/odoc/odoc_scope.ml
@@ -1,56 +1,83 @@
 open Import
 open Memo.O
 
-module Scope_key = struct
-  let file_key project =
-    let name = Dune_project.name project in
-    let root = Dune_project.root project in
-    let digest =
-      Digest.repr Repr.(pair Dune_project_name.repr Path.Source.repr) (name, root)
-      |> Digest.to_string
+let file_key project =
+  let name = Dune_project.name project in
+  let root = Dune_project.root project in
+  let digest =
+    Digest.repr Repr.(pair Dune_project_name.repr Path.Source.repr) (name, root)
+    |> Digest.to_string
+  in
+  String.take digest 12
+;;
+
+let find_project_by_key =
+  let memo =
+    let make_map projects =
+      String.Map.of_list_map_exn projects ~f:(fun project -> file_key project, project)
+      |> Memo.return
     in
-    String.take digest 12
-  ;;
+    let module Input = struct
+      type t = Dune_project.t list
 
-  let find_project_by_key =
-    let memo =
-      let make_map projects =
-        String.Map.of_list_map_exn projects ~f:(fun project -> file_key project, project)
-        |> Memo.return
-      in
-      let module Input = struct
-        type t = Dune_project.t list
-
-        let equal = List.equal Dune_project.equal
-        let hash = List.hash Dune_project.hash
-        let to_dyn = Dyn.list Dune_project.to_dyn
-      end
-      in
-      Memo.create "project-by-keys" ~input:(module Input) make_map
+      let equal = List.equal Dune_project.equal
+      let hash = List.hash Dune_project.hash
+      let to_dyn = Dyn.list Dune_project.to_dyn
+    end
     in
-    fun key ->
-      let* projects = Dune_load.projects () in
-      let+ map = Memo.exec memo projects in
-      String.Map.find_exn map key
-  ;;
+    Memo.create "project-by-keys" ~input:(module Input) make_map
+  in
+  fun key ->
+    let* projects = Dune_load.projects () in
+    let+ map = Memo.exec memo projects in
+    String.Map.find_exn map key
+;;
 
-  let of_string context s =
+module Scope_id = struct
+  type t =
+    | Package of Package.Name.t
+    | Private_lib of
+        { unique_name : string
+        ; lib_name : Lib_name.t
+        ; project : Dune_project.t
+        }
+
+  let of_string s =
     match String.rsplit2 s ~on:'@' with
-    | None ->
-      let+ public_libs = Scope.DB.public_libs context in
-      Lib_name.parse_string_exn (Loc.none, s), public_libs
+    | None -> Memo.return (Package (Package.Name.of_string s))
     | Some (lib, key) ->
-      let+ scope = find_project_by_key key >>= Scope.DB.find_by_project context in
-      Lib_name.parse_string_exn (Loc.none, lib), Scope.libs scope
+      let+ project = find_project_by_key key in
+      let lib_name = Lib_name.parse_string_exn (Loc.none, lib) in
+      Private_lib { unique_name = s; lib_name; project }
   ;;
 
+  let to_string = function
+    | Package pkg -> Package.Name.to_string pkg
+    | Private_lib { unique_name; _ } -> unique_name
+  ;;
+
+  let is_private_lib = function
+    | Package _ -> false
+    | Private_lib _ -> true
+  ;;
+
+  let as_package_name = function
+    | Package pkg -> pkg
+    | Private_lib { unique_name; _ } -> Package.Name.of_string unique_name
+  ;;
+end
+
+module Scope_key : sig
+  val to_string : Lib_name.t -> Dune_project.t -> string
+end = struct
   let to_string lib project =
     let key = file_key project in
     sprintf "%s@%s" (Lib_name.to_string lib) key
   ;;
 end
 
-let lib_unique_name lib =
+let lib_unique_name (local_lib : Lib.Local.t) =
+  let lib = Lib.Local.to_lib local_lib in
   let name = Lib.name lib in
   let info = Lib.info lib in
   let status = Lib_info.status info in
@@ -58,10 +85,4 @@ let lib_unique_name lib =
   | Installed_private | Installed -> assert false
   | Public _ -> Lib_name.to_string name
   | Private (project, _) -> Scope_key.to_string name project
-;;
-
-let pkg_or_lnu lib =
-  match Lib_info.package (Lib.info lib) with
-  | Some p -> Package.Name.to_string p
-  | None -> lib_unique_name lib
 ;;
--- src/dune_rules/odoc/odoc_scope.mli
+++ src/dune_rules/odoc/odoc_scope.mli
@@ -1,24 +1,59 @@
+(** Scope and naming utilities for odoc v2 library disambiguation *)
+
 open Import
+
+(** Identifies a scope for documentation generation - either a package or a private library.
+
+    This type provides validated scope identification. For private libraries,
+    the project is resolved and validated during construction, ensuring that
+    if you have a [Scope_id.t], it refers to a valid scope. *)
+module Scope_id : sig
+  type t =
+    | Package of Package.Name.t
+    | Private_lib of
+        { unique_name : string
+        ; lib_name : Lib_name.t
+        ; project : Dune_project.t
+        }
+
+  (** Parse and validate a scope ID string.
+
+      For strings containing '@' (private library format "libname@projectkey"),
+      this resolves the project from the key and validates it exists.
+      For other strings, treats them as package names.
+
+      Returns a [Memo.t] because private library validation requires
+      looking up the project by its key. *)
+  val of_string : string -> t Memo.t
+
+  (** Convert to string for use in paths. *)
+  val to_string : t -> string
+
+  (** Check if this is a private library (vs a regular package). *)
+  val is_private_lib : t -> bool
+
+  (** Get as a Package.Name.t. For private libs, this parses the unique_name
+      as a package name (useful for certain operations that need a Package.Name.t). *)
+  val as_package_name : t -> Package.Name.t
+end
 
 (** Scope key encoding for v2 library names.
 
-    Private libraries from different projects need unique identifiers to avoid
-    name collisions. The [Scope_key] module encodes project information into
-    library names using the format "libname@key" where [key] is a 12-character
+    In odoc v2, private libraries from different projects need unique identifiers
+    to avoid name collisions. The Scope_key module encodes project information
+    into library names using the format "libname@key" where key is a 12-character
     hash derived from the project name and root path. *)
 module Scope_key : sig
-  (** Parse a possibly scope-encoded library name, returning the library name
-      and the [Lib.DB.t] it should be looked up in. *)
-  val of_string : Context_name.t -> string -> (Lib_name.t * Lib.DB.t) Memo.t
+  (** Convert a library name to its v2 unique name format.
 
-  (** Encode a private library name for the given project as "libname@key". *)
+      For a private library in the given project, generates "libname@key" where
+      key is a 12-character hash uniquely identifying the project. *)
   val to_string : Lib_name.t -> Dune_project.t -> string
 end
 
-(** Unique name for a library: its plain name for public libraries, the v2
-    "libname@key" format for private libraries. Raises on installed libraries. *)
-val lib_unique_name : Lib.t -> string
+(** Generate a unique name for a local library.
 
-(** Directory name for a library's documentation: its package name if it has
-    one, otherwise its [lib_unique_name]. *)
-val pkg_or_lnu : Lib.t -> string
+    - Public libraries: use their plain name
+    - Private libraries: use v2 format "libname@key" from Scope_key.to_string
+    - Raises: assertion failure if called on installed libraries *)
+val lib_unique_name : Lib.Local.t -> string
