a425f9ed4d9de2e8df2caa5170f521fec13a2064
Paul-Elliot <peada@free.fr>
2026-06-02 18:24:11 +0200

    feat(odoc): redesign Odoc_target as a v3 GADT of documentation targets

--- src/dune_rules/odoc/odoc_target.mli
+++ src/dune_rules/odoc/odoc_target.mli
@@ -1,6 +1,32 @@
 open Import
 
-(** A documentation target: either a local library or a package. *)
-type t =
-  | Lib of Lib.Local.t
-  | Pkg of Package.Name.t
+module Doc_mode : sig
+  type t =
+    | Local_only
+    | Full
+
+  val subdir : t -> string
+  val all : t list
+end
+
+type page =
+  { name : string
+  ; pkg_libs : Lib.t list
+  }
+
+type mod_ =
+  { visible : bool
+  ; module_name : Module_name.t
+  }
+
+type _ t =
+  | Lib : Package.Name.t * Lib.t -> mod_ t
+  | Private_lib : string * Lib.t -> mod_ t
+  | Pkg : Package.Name.t -> page t
+  | Toplevel : Doc_mode.t -> page t
+
+(** Existential wrapper for targets of any kind. *)
+type any = Any : 'a t -> any
+
+(** Compare two targets for sorting. Orders by target type then by name. *)
+val compare_any : any -> any -> Ordering.t
